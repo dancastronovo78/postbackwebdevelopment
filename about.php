295a6ffb36294ae00221bca172dfc922d275911e
Daniel Castronovo <dancastronovo@yahoo.com>
2019-02-03 19:04:55 -0600

    uploaded files

---
+++ about.php
@@ -0,0 +1,95 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <title>Who is Postback?</title>
+        <meta charset="utf-8">
+        <link href="css/about.css" rel="stylesheet" />
+        <link href="css/nav.css" rel="stylesheet" />
+        <link href="css/social.css" rel="stylesheet" />
+        <link href="css/navMediaQuery.css" rel="stylesheet" />
+        <link href="css/aboutMediaQuery.css" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css?family=Montserrat:100" rel="stylesheet">
+         <link href="https://fonts.googleapis.com/css?family=Muli:200" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+         <meta name="description" content="Looking for dynamic designs for web development, web video production, graphic design, and eLearning development, Postback Web Development is here for you! Get to know the web developer and designer Daniel Castronovoand.">
+        <meta name="keywords"   content="Postback, Postback Austin, Postback Austin Texas, Postback Austin TX,  Postback Web Development, Postback Web Development Austin, Postback Web Development Austin Texas, Postback Web Development Austin TX, Daniel Castronovo, Daniel Castronovo Austin, Daniel Castronovo Austin Texas, Daniel Castronovo Austin TX, Freelance, Freelance Web Developer, Freelance Web developer Austin, Freelance Web developer Austin Texas, Freelance Web developer Austin TX, Daniel Castronovo Freelance Web Developer, Daniel Castronovo Freelance Web developer Austin, Daniel Castronovo Freelance Web developer Austin Texas, Daniel Castronovo Freelance Web developer Austin TX,  Web development, Web development Austin, Web development Austin Texas, Web development Austin TX, Web design, Web design Austin, Web design Austin Texas, Web design Austin TX, Front end development, Front end development Austin, Front end development Austin Texas, Front end development Austin TX, Back end development, Back end development Austin, Back end development Austin Texas, Back end development Austin TX, Fullstack development, Fullstack development Austin, Fullstack development Austin Texas, Fullstack development Austin TX, JavaScript, Web designer, Web designer Austin, Web designer Austin Texas, Web designer Austin TX, Front end developer, Front end developer Austin, Front end developer Austin Texas, Front end developer Austin TX, Back end developer, Back end developer Austin, Back end developer Austin Texas, Back end developer Austin TX, Fullstack developer, Fullstack developer Austin, Fullstack developer Austin Texas, Fullstack developer Austin TX, Full stack developer, Full stack developer Austin, Full stack developer Austin Texas, Full stack developer Austin TX, Full stack development, Full stack development Austin, Full stack development Austin Texas, Full stack development Austin TX, ASP, PHP, jQuery, MySql, SQL, Graphic design, Web video, Web video production, Web video design, Web video development, elearning, E-Learing, eLearning, ELearning,">
+        <script src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src = "https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js"></script>
+        <script src="js/nav.js"></script>
+      <script src="js/scrollTop.js"></script>
+    </head>
+    <body>
+        <header>
+            <?php include 'includes/header.php';?>
+        </header>
+        <div id="titleH1"><h1 tabindex="1">Who is Postback?</h1></div>
+          <!--Main Container-->
+        <div>
+            <div id="topAnchor"></div>
+        <main id="mainContainer">
+            <div id="col1" class="col4">
+                <div class="emptyDiv1">
+                    <div class="modalVid">
+                            <video controls>
+                              <source src="video/IntroductionVideo.mp4" type="video/mp4">
+                              Your browser does not support the video tag.
+                            </video>
+                        </div>
+                    <div id="misArrow">
+                        <a id="AncArrow" href="#msnAnchor"><img id="arrow" src="images/arrowNext.png" alt="Up/Down Arrows"></a>
+                    </div>
+                   <!--<div id="misArrow">
+                        <a href="#misAnc"><img id="arrow" src="images/arrowNext.png" alt="Up/Down Arrows"></a>
+                    </div>-->
+                </div>
+            </div>
+            <div id="col2" class="col4">
+                 <div id="msnAnchor"></div>
+                 <div id="misAnc" class="emptyDiv2">
+                <h2 class="cardH2" tabindex="1">Mission Statement</h2>
+                 <p class="card2">It is not only our mission to develop dynamic web solutions, but to also build dynamic relationships with our clients. We believe that partnerships are essential in successful design and development projects. Together we will create transformative results. </p>
+                </div>
+            </div>
+            <div id="col3" class="col4">
+                 <div id="proAnc" class="emptyDiv3">
+                <h2 class="cardH2" tabindex="1">Designer Profile</h2>
+                 <p class="card3">Daniel is a Web Developer with experience overseeing the development and evaluation of educational and computer-based training. He is broadly and deeply knowledgeable in a wide variety of computer languages as well as the principles and techniques of website construction and maintenance. Daniel is an adept multitasker with the ability to bring simultaneous design and development projects to completion, with efficiency and accuracy.  He is able to work well independently, under limited supervision, or as a member of a design team, to create desired web outcomes and solutions.</p>
+                </div>
+            </div>
+            <div id="col4" class="col4">
+                 <div id="skiAnc" class="emptyDiv4">
+                      <div id="misArrowUp">
+                        <a id="AncArrow2" href="#topAnchor"><img id="arrowUp" src="images/arrowNext.png" alt="Up/Down Arrows"></a>
+                    </div>
+                <h2 class="cardH2" tabindex="1">Skill Sets</h2>
+                 <p class="card4">HTML5, CSS3, JavaScript, PHP, MySQL, ASP, MS SQL, Visual Studio 2017, NetBeans, Dreamweaver, Notepad++, XAMPP, Apache, Adobe CC, Photoshop, Illustrator, Premier Pro, Camtasia, SoftChalk, MS Office</p>
+                </div>
+            </div>
+        </main>
+       </div>
+          <footer tabindex="1">
+            <?php include 'includes/footer.php';?>
+          </footer>
+          <div id="social">
+                <?php include 'includes/social.php'; ?>
+            </div>
+    </body> 
+</html>
